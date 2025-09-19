11e64b218aa900c690f9e0af19b642b8bfdea3eb
Omkar <omkarbarge18@gmail.com>
2025-09-19 17:34:20 +0530

    added time code with fix

--- index.js
+++ index.js
@@ -14,23 +14,19 @@ function showTime() {
     let sec = time.getSeconds();
     let am_pm = "AM";
 
-    // Convert to 12-hour format
     if (hour >= 12) {
         if (hour > 12) hour -= 12;
         am_pm = "PM";
     } else if (hour === 0) {
         hour = 12;
-        am_pm = "AM";
     }
 
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
 
-    let currentTime = `${hour}:${min}:${sec} ${am_pm}`;
-    clock.textContent = currentTime;
+    clock.textContent = `${hour}:${min}:${sec} ${am_pm}`;
 }
 
-// Update clock every second
 setInterval(showTime, 1000);
 showTime();
